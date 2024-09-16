554e4ace90503657abc09d0e04c40130e24c0aaf
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-16 15:09:18 -0400

    feat: add keymap for sending items to quick fix list

--- lua/raman/plugins/core/telescope.lua
+++ lua/raman/plugins/core/telescope.lua
@@ -17,7 +17,7 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous,
 						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						-- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
 			},
