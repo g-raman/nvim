26af0bd35b2fe2f1e64f258ea0c848585652b528
Raman Gupta <gr.gupta.raman@gmail.com>
2024-09-29 17:53:38 -0400

    feat: auot select for cmp

--- lua/raman/plugins/core/nvim-cmp.lua
+++ lua/raman/plugins/core/nvim-cmp.lua
@@ -43,12 +43,11 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(),
 				["<C-j>"] = cmp.mapping.select_next_item(),
-
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-c>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
