12e68b236a2fb9feeb89184bb1c48fc3fdc074fd
Raman Gupta <gr.gupta.raman@gmail.com>
2025-09-21 14:04:31 -0400

    refactor: simplify lsp setup

--- lua/raman/config/lsp.lua
+++ lua/raman/config/lsp.lua
@@ -1,58 +1,60 @@
 local servers = require("raman.config.servers")
 
+local lsp_highlight_group = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
+local lsp_attach_group = vim.api.nvim_create_augroup("lsp-attach", { clear = true })
+local lsp_detach_group = vim.api.nvim_create_augroup("lsp-detach", { clear = true })
+
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	group = lsp_attach_group,
 	callback = function(event)
 		local map = function(keys, func, desc, mode)
 			mode = mode or "n"
 			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 		end
+		local buf = event.buf
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
 		local fzf = require("fzf-lua")
 
-		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
-		map("gR", fzf.lsp_references, "[G]oto [R]eferences")
-
-		map("gi", fzf.lsp_implementations, "[G]oto [I]mplementation")
-
+		-- Navigation
 		map("gd", fzf.lsp_definitions, "[G]oto [D]efinition")
-
 		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
+		map("gi", fzf.lsp_implementations, "[G]oto [I]mplementation")
+		map("gR", fzf.lsp_references, "[G]oto [R]eferences")
 		map("gt", fzf.lsp_typedefs, "[G]oto [T]ype Definition")
 
+		-- Actions
+		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+		-- Info
+		map("K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
 		map("<leader>d", vim.diagnostic.open_float, "Open [D]iagnostic")
 
-		map("K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
-
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-			local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+		if client and client.server_capabilities.documentHighlightProvider then
 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
+				group = lsp_highlight_group,
+				buffer = buf,
 				callback = vim.lsp.buf.document_highlight,
 			})
 
 			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
+				group = lsp_highlight_group,
+				buffer = buf,
 				callback = vim.lsp.buf.clear_references,
 			})
+
 			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-				callback = function(event2)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+				group = lsp_detach_group,
+				callback = function(ev)
+					pcall(vim.lsp.buf.clear_references, { bufnr = ev.buf })
+					vim.api.nvim_clear_autocmds({ group = lsp_highlight_group, buffer = ev.buf })
 				end,
 			})
 		end
 
-		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+		if client and client.server_capabilities.inlayHintProvider then
 			map("<leader>th", function()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = buf }), { bufnr = buf })
 			end, "[T]oggle Inlay [H]ints")
 		end
 	end,
