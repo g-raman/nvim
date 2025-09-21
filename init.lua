abd1008a8ebc111778ddf14d693c4d2b8b50077c
Raman Gupta <gr.gupta.raman@gmail.com>
2025-09-21 13:48:54 -0400

    refactor: assign leader key in init.lua and change ordering of imports

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
-require("raman.config.settings")
-require("raman.config.keymaps")
+vim.g.mapleader = " "
 require("raman.lazy")
+require("raman.config.keymaps")
+require("raman.config.settings")
 require("raman.config.ui")
 require("raman.config.lsp")
--- lua/raman/config/keymaps.lua
+++ lua/raman/config/keymaps.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<Esc>")
