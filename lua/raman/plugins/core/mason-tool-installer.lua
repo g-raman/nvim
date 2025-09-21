817c57fcd1849169b8ed97eab076a60bff3ef544
Raman Gupta <gr.gupta.raman@gmail.com>
2025-09-21 12:57:39 -0400

    feat: update mason-tool-installer to import list of servers

--- lua/raman/plugins/core/mason-tool-installer.lua
+++ lua/raman/plugins/core/mason-tool-installer.lua
@@ -1,26 +1,19 @@
+local servers = require("raman.config.servers")
+local ensure_installed = vim.list_extend(servers, {
+	"stylua",
+	"prettierd",
+	"prettier",
+	"clang-format",
+})
+
 return {
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
 	dependencies = {
 		{ "williamboman/mason.nvim", opts = {} },
+		"williamboman/mason-lspconfig.nvim",
 		{ "j-hui/fidget.nvim", opts = {} },
 	},
 	opts = {
-		ensure_installed = {
-			-- LSP
-			"bash-language-server",
-			"css-lsp",
-			"eslint-lsp",
-			"html-lsp",
-			"json-lsp",
-			"lua-language-server",
-			"pyright",
-			"tailwindcss-language-server",
-
-			-- Formatters
-			"stylua",
-			"prettierd",
-			"prettier",
-			"clang-format",
-		},
+		ensure_installed = ensure_installed,
 	},
 }
