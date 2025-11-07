4038db52d892d5f452a77372f2601bf81ca40352
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-06 19:45:15 -0500

    feat: add autocmd to save file cursor position

---
+++ lua/raman/config/autocmds.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.api.nvim_win_set_cursor(0, mark)
+
+			-- Defer centering slightly so it's applied after render
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
